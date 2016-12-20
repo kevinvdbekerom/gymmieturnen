f65a2ef215cb3e57f5ee7d18eb51ac1ca72a4f4d
Kevin van den Bekerom <kevin92.bekerom@live.nl>
2016-12-20 22:25:20 +0100

    web server setup

--- lib/app/controllers/club_controller.rb
+++ lib/app/controllers/club_controller.rb
@@ -1,11 +1,19 @@
-require 'sinatra'
+require 'sinatra/base'
 
-class ClubController
+class ClubController < Sinatra::Application
+
+  puts (settings.root + '/../views')
+
+  set :root, File.dirname(__FILE__)
+  set :views, "#{settings.root}/../views/*.erb"
 
   # Home page
   get '/' do
     erb :index
   end
 
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 
 end
